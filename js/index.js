a54f10d531fa224aa81263cca22a8ef872d05b1d
RSConcordia <renansconcordia@gmail.com>
2016-03-21 10:45:14 -0300

    Sample I

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 	var app = {	
 		
 	};	
-	function geolocation(){ 
-			alert('start');
+	function geolocation(){
 			var watchId = navigator.geolocation.watchPosition(onSuccess, onError, { enableHighAccuracy: true });
-		};	
+	}
+		
 	function onSuccess(position) {
 		var element = document.getElementById('geolocation');
 		element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
 							'Longitude: ' + position.coords.longitude     + '<br />' +
 							'Speed: ' 	  + position.coords.speed         + '<br />' +
 							'<hr />' ;
-	};					
+	}
+	
 	function onError(error) {
-		alert('code: '    + error.code    + '\n' +
-			  'message: ' + error.message + '\n');
-	};
+		var element = document.getElementById('geolocation');
+		element.innerHTML = 'code: '    + error.code  + '<br>'+ 
+							'message: ' + error.message ;
+	}
